f32422b0e9190c065e9f92f839259aac9d0cfe8a
arif6474 <arifulislam64743@gmail.com>
2025-11-16 02:23:31 +0600

    comments/OptionB

--- socket.js
+++
@@ -1,8 +0,0 @@
-import { io } from "socket.io-client";
-
-// Initialize the Socket.IO connection
-// Use environment variable for backend URL
-const SOCKET_URL = process.env.NEXT_PUBLIC_API_URL || "http://localhost:5000";
-const socket = io(SOCKET_URL);
-
-export default socket;
---
+++ socket.ts
@@ -0,0 +1,8 @@
+import { io } from "socket.io-client";
+
+// Initialize the Socket.IO connection
+// Use environment variable for backend URL
+const SOCKET_URL = process.env.NEXT_PUBLIC_BACKEND_URL || "http://localhost:5000";
+const socket = io(SOCKET_URL);
+
+export default socket;
